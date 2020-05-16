49c2ab25167be0f9a8f62d11d42eba1594e5b41e
o.tymchuk <o.tymchuk@krok.edu.ua>
2020-05-16 13:33:58 +0300

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\o.tymchuk\Google Диск\courses\prog.kiev.ua\lecture_16\example\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
